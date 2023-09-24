c2cf74cb31f42660f49f4c54717acfff8e7c711e
kohei-okazaki <kou1210hei@gmail.com>
2023-09-24 11:35:23 +0900

    フォーマッター適用

--- kintai/static/js/app/dailywork.js
+++ kintai/static/js/app/dailywork.js
@@ -1,28 +1,24 @@
-$(window).on('load', function () {
-    if ($("#dailywork_create").length) {
-        $.each($("div.weekday"), function (i, val) {
-
-            if ($(val).text() === "土") {
-                // 土曜日の場合
-                $(val).addClass("saturday");
-                $(".saturday").css({
-                    "color": "#0000ff"
-                });
-
-            } else if ($(val).text() === "日" || $(val).text() === "祝") {
-                // 日曜日の場合
-                $(val).addClass("sunday");
-                $(".sunday").css({
-                    "color": "#ff0000"
-                });
-
-            } else {
-                // 平日の場合
-                $(val).addClass("workday");
-            }
+$(window).on("load", function () {
+  if ($("#dailywork_create").length) {
+    $.each($("div.weekday"), function (i, val) {
+      if ($(val).text() === "土") {
+        // 土曜日の場合
+        $(val).addClass("saturday");
+        $(".saturday").css({
+          color: "#0000ff",
         });
-        $("div.contents-menu").css("height", "260%");
-        $("div.contents-submenu").css("height", "260%");
-    }
-
+      } else if ($(val).text() === "日" || $(val).text() === "祝") {
+        // 日曜日の場合
+        $(val).addClass("sunday");
+        $(".sunday").css({
+          color: "#ff0000",
+        });
+      } else {
+        // 平日の場合
+        $(val).addClass("workday");
+      }
+    });
+    $("div.contents-menu").css("height", "260%");
+    $("div.contents-submenu").css("height", "260%");
+  }
 });
